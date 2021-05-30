5cce6777612ead93157c763051e87de5bd817b5a
guilhermercarvalho <guilhermercarvalho512@gmail.com>
2021-05-30 13:14:56 -0300

    add image app remove support

--- .devcontainer/script/devcontainer_prune.sh
+++ .devcontainer/script/devcontainer_prune.sh
@@ -1,8 +1,11 @@
 #!/usr/bin/env sh
 
-echo -e "removing containers...\n"
+printf "removing containers...\n"
 docker container stop java-APSOO-Venda-Carros postgres-dev-APSOO-Venda-Carros postgres-test-APSOO-Venda-Carros
 docker container rm java-APSOO-Venda-Carros postgres-dev-APSOO-Venda-Carros postgres-test-APSOO-Venda-Carros
 
-echo -e "\nremoving volumes...\n"
+printf "\nremoving volumes...\n"
 docker volume prune -f
+
+printf "\nremoving images...\n"
+docker image rm ufms-apsoo-venda-carros_devcontainer_app
