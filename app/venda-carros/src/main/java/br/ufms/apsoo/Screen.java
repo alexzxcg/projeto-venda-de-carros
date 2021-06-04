431578fe6fbd0988c9900f66fb54246fa31fc609
guilhermercarvalho <guilhermercarvalho512@gmail.com>
2021-06-04 18:56:22 +0000

    add screen application

---
+++ app/venda-carros/src/main/java/br/ufms/apsoo/Screen.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.ufms.apsoo;
+
+import br.ufms.apsoo.screens.Telas.Tela;
+
+/**
+ *
+ * @author alexlinux
+ */
+public class Screen {
+    public static void main(String[] args) {
+        Tela tela = new Tela();
+        tela.setVisible(true);
+    }
+    
+}
